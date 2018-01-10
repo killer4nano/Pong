3d1739700f259c6c37a79800ee2de44baa279fc7
killer4nano <alikhaliq94@gmail.com>
2018-01-10 15:10:07 -0500

    final

--- WindowsApplication1/Game.vb
+++ WindowsApplication1/Game.vb
@@ -7,10 +7,10 @@ Public Class Game
     Dim bw As IO.BinaryWriter
     Dim br As IO.BinaryReader
     Dim ipAddress As IPAddress
-    Dim host As String = System.Net.Dns.GetHostName()
 
+    Private communicationThread As Thread
     Dim connection As TcpClient
-    Dim LocalHostaddress As String
+
 
     Private Sub Game_Closwe(sender As Object, e As EventArgs) Handles MyBase.FormClosed
         HomeScreen.Show()
@@ -19,7 +19,8 @@ Public Class Game
     Private Sub Game_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim hostIp As String
-        LocalHostaddress = System.Net.Dns.GetHostEntry(host).AddressList(0).ToString()
+        Dim host As String = System.Net.Dns.GetHostName()
+        Dim LocalHostaddress As String = System.Net.Dns.GetHostEntry(host).AddressList(0).ToString()
 
         If isHost Then
             lblIP.Text = "Give this Ip to your friend " + LocalHostaddress
@@ -27,36 +28,55 @@ Public Class Game
         Else
             hostIp = InputBox("What is the host IP?", "Connection", "")
             lblIP.Text = "Host IP : " + hostIp
-            ipAddress = Dns.GetHostEntry(hostIp).AddressList(0)
+
         End If
 
         If isHost Then
             'Start the server listening port
+            ipAddress = Dns.GetHostEntry(LocalHostaddress).AddressList(0)
+            Dim listener As New TcpListener(ipAddress, 49552)
+            listener.Start()
+            connection = listener.AcceptTcpClient()
 
         Else
             'Connect to the server
             connection = New TcpClient
-            connection.Connect(ipAddress.ToString, 49552)
+            connection.Connect(hostIp, 49552)
 
 
         End If
-        
 
-    End Sub
-    Private Sub Game_Loaded(sender As Object, e As EventArgs) Handles MyBase.Validated
-        If isHost Then
-            getConnection()
-        End If
         bw = New IO.BinaryWriter(connection.GetStream())
         br = New IO.BinaryReader(connection.GetStream())
+        communicationThread = New Thread(AddressOf communicationStart)
+
     End Sub
 
-    Private Sub getConnection()
-        IPAddress = Dns.GetHostEntry(LocalHostaddress).AddressList(0)
-        Dim listener As New TcpListener(IPAddress, 49552)
-        listener.Start()
-        connection = listener.AcceptTcpClient()
+
+    Private Sub communicationStart()
+
+        Do While True
+
+            Dim message As String
+            message = br.ReadString
+            MsgBox(message)
+            If isHost Then
+
+                If message = "U" Then
+                    objPlayer2.SetBounds(objPlayer2.Left, objPlayer2.Top + 15, 25, 130)
+                ElseIf message = "D" Then
+                    objPlayer2.SetBounds(objPlayer2.Left, objPlayer2.Top + 15, 25, 130)
+                End If
+            Else
+                If message = "U" Then
+                    objPlayer1.SetBounds(objPlayer1.Left, objPlayer1.Top + 15, 25, 130)
+                ElseIf message = "D" Then
+                    objPlayer1.SetBounds(objPlayer1.Left, objPlayer1.Top + 15, 25, 130)
+                End If
+            End If
+        Loop
     End Sub
+
     Private Sub Game_Load(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
         If isHost Then
             If e.KeyCode = Keys.Down Then
@@ -69,10 +89,10 @@ Public Class Game
 
         Else
             If e.KeyCode = Keys.Down Then
-                objPlayer1.SetBounds(objPlayer2.Left, objPlayer2.Top + 15, 25, 130)
+                objPlayer2.SetBounds(objPlayer2.Left, objPlayer2.Top + 15, 25, 130)
                 bw.Write("D")
             ElseIf e.KeyCode = Keys.Up Then
-                objPlayer1.SetBounds(objPlayer2.Left, objPlayer2.Top - 15, 25, 130)
+                objPlayer2.SetBounds(objPlayer2.Left, objPlayer2.Top - 15, 25, 130)
                 bw.Write("U")
             End If
         End If
